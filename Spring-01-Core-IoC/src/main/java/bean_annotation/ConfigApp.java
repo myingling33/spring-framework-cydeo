77c1fe453c7d6c48908a4436380a0ceed736a7ee
Michael Yingling <myingling33@gmail.com>
2023-09-04 09:12:14 -0500

    Multiple Beans Same Type

--- Spring-01-Core-IoC/src/main/java/bean_annotation/ConfigApp.java
+++ Spring-01-Core-IoC/src/main/java/bean_annotation/ConfigApp.java
@@ -2,6 +2,7 @@ package bean_annotation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // When you start the application, Spring is going to find the classes
 // with the @Configuration, process the bean creation, and put it in
@@ -14,8 +15,16 @@ public class ConfigApp {
         return new FullTimeMentor();
     }
 
+    // @Bean(name = "p1")
     @Bean
+    @Primary
     PartTimeMentor partTimeMentor() {
+        return new PartTimeMentor();
+    }
+
+    // @Bean(name = "p2")
+    @Bean
+    PartTimeMentor partTimeMentor2() {
         return new PartTimeMentor();
     }
 }
