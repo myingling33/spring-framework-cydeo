3bc2ef110e731ab381a17d57c7950d38c0e1ed6e
Michael Yingling <myingling33@gmail.com>
2023-09-05 10:16:23 -0500

    Wiring

--- Spring-01-Core-IoC/src/main/java/stereotype_annotation/CydeoApp.java
+++ Spring-01-Core-IoC/src/main/java/stereotype_annotation/CydeoApp.java
@@ -7,6 +7,6 @@ public class CydeoApp {
     public static void main(String[] args) {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(ConfigCourse.class);
-
+        context.getBean(Java.class).getTeachingHours();
     }
 }
