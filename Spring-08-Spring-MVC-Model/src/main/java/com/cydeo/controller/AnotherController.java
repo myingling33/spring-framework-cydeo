9d3647dd025f03fbb25be02028a82263b3035d86
Michael Yingling <myingling33@gmail.com>
2023-09-11 20:48:23 -0500

    Spring MVC

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/AnotherController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/AnotherController.java
@@ -10,10 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/mentor")
-public class MentorController {
+@RequestMapping("/another")
+public class AnotherController {
 
-    @RequestMapping("/list")  // localhost:8080/mentor/list
+    @RequestMapping("/list") // localhost:8080/another/list
     public String showTable(Model model) {
         List<Mentor> mentorList = new ArrayList<>();
         mentorList.add(new Mentor("Mike", "Smith", 45, Gender.MALE));
