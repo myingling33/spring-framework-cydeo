348850d621856ddcb4da15b47e9e965049b02f66
Michael Yingling <myingling33@gmail.com>
2023-10-26 07:04:49 -0500

    ORM Part-2

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -1,12 +1,16 @@
 package com.cydeo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Employee { //employee
 
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
 }
