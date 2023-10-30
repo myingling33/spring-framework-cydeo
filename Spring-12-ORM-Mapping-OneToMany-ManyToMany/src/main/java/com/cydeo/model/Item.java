1d38aa5460959accfe5bab3afcb431cba4c8fb20
Michael Yingling <myingling33@gmail.com>
2023-10-30 12:27:15 -0500

    Spring ORM Part-4 Mapping

---
+++ Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Item.java
@@ -0,0 +1,25 @@
+package com.cydeo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "items")
+@Data
+@NoArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    @ManyToMany
+    private List<Cart> cart;
+
+}
