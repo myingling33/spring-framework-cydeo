38380ad800d54ac93210e77b027af36a8c145a04
Michael Yingling <myingling33@gmail.com>
2023-11-08 11:07:39 -0600

    Queries

---
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -0,0 +1,29 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface CourseRepository extends JpaRepository<Course,Long> {
+
+    // Find all courses by category - Derive Query
+    List<Course> findByCategory(String category);
+
+    // Find all courses by category and order the entities by name
+    List<Course> findByCategoryOrderByName(String category);
+
+    // Checks if a course with the provided name exists; return true
+    // if course exists, false otherwise
+    boolean existsByName(String name);
+
+    // Returns the count of courses for the provided category
+    int countByCategory(String category);
+
+    // Find all courses that start with the provided course name
+    List<Course> findByNameStartsWith(String name);
+
+    // Find all courses by category and returns a stream
+    Stream<Course> streamByCategory(String category);
+}
