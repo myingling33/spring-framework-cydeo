456495e3f6c5cb81c369f35e12f173bd0163ed60
Michael Yingling <myingling33@gmail.com>
2023-12-03 13:26:38 -0600

    CinemaRepository Derived queries completed.

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -1,7 +1,25 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Cinema;
+import com.cydeo.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CinemaRepository extends JpaRepository<Cinema,Long> {
+
+    // ----------------------- DERIVED QUERIES ------------------ //
+
+    // To get cinema with a specific name
+    Optional<Cinema> findByName(String name);
+
+    // To read sorted the top 3 cinemas that contain a specific sponsored name
+    List<Cinema> findTop3BySponsoredNameContainingOrderBySponsoredName(String sponsoredName);
+
+    // To list all cinemas in a specific country
+    List<Cinema> findByLocation_Country(String country);
+
+    // To list all cinemas with a specific name or sponsored name
+    List<Cinema> findByNameOrSponsoredName(String name, String sponsoredName);
 }
