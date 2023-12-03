ded8ab812c4171a717199d54184f5a7ea26a147c
Michael Yingling <myingling33@gmail.com>
2023-12-03 13:55:54 -0600

    CinemaRepository Native queries completed.

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -33,25 +33,21 @@ public interface CinemaRepository extends JpaRepository<Cinema,Long> {
     // ----------------------- Native QUERIES ------------------ //
 
     // To read all cinemas by location country.
+    @Query(value = "SELECT * FROM cinema c JOIN location l" +
+            " ON c.location_id = l.id WHERE l.country = ?1", nativeQuery = true)
+    List<Cinema> retrieveAllBasedOnLocationCountry(String country);
 
     // To read all cinemas by name or sponsored name contains a specific pattern
+    @Query(value = "SELECT * FROM cinema WHERE name ILIKE concat('%', ?1, '%') OR " +
+            " sponsored_name ILIKE concat('%', ?1, '%') ", nativeQuery = true)
+    List<Cinema> retrieveAllByNameOrSponsoredName(String pattern);
 
     // To sort all cinemas by name
+    @Query(value = "SELECT * FROM cinema ORDER BY name", nativeQuery = true)
+    List<Cinema> sortByName();
 
     // To distinct all cinemas by sponsored name
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @Query(value = "SELECT DISTINCT sponsored_name FROM cinema", nativeQuery = true)
+    List<String> distinctBySponsoredName();
 
 }
