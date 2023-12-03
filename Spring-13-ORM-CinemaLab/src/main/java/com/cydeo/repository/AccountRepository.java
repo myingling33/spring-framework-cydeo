58b43a107dd81796539bac94629d2bbe4b49568c
Michael Yingling <myingling33@gmail.com>
2023-12-03 09:13:03 -0600

    AccountRepository derived queries completed.

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.cydeo.repository;
 
 import com.cydeo.entity.Account;
+import com.cydeo.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,18 +13,23 @@ public interface AccountRepository extends JpaRepository<Account,Long> {
     // ----------------------- DERIVED QUERIES ------------------ //
 
     // Write a derived query to list all accounts with a specific country or state
-    List<Account> findByCountryOrState(String countryState);
+    List<Account> findByCountryOrState(String country, String state);
 
     // Write a derived query to list all accounts with age lower than or equal to a specific value
+    List<Account> findByAgeLessThanEqual(Integer age);
 
     // Write a derived query to list all accounts with a specific role
+    List<Account> findByRole(UserRole role);  // role is an Enum
 
     // Write a derived query to list all accounts between a range of ages
+    List<Account> findByAgeBetween(Integer ageStart, Integer ageEnd);
 
     // Write a derived query to list all accounts where the beginning of the address
     // contains the keyword
+    List<Account> findByAddressStartsWith(String keyword);
 
     // Write a derived query to sort the list of accounts with age
+    List<Account> findByOrderByAgeDesc();
 
 
 
