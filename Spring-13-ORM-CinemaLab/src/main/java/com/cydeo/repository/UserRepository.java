348c5d6f50ab229b2de5bf8d9d4dc484e512ab68
Michael Yingling <myingling33@gmail.com>
2023-12-05 17:14:00 -0600

    UserRepository Native queries completed.

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/UserRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/UserRepository.java
@@ -44,5 +44,21 @@ public interface UserRepository extends JpaRepository<User,Long> {
     // ----------------------- Native QUERIES ------------------ //
 
     // Returns all users that contain a specific name.
+    @Query(value = "SELECT * FROM user_account ua JOIN account_details ad " +
+            " ON ua.account_details_id = ad.id WHERE ad.name ILIKE concat('%', ?1, '%')", nativeQuery = true)
+    List<User> retrieveAllByName(String name);
 
+    // Returns all users.
+    @Query(value = "SELECT * FROM user_account", nativeQuery = true)
+    List<User> retrieveAll();
+
+    // Returns all users in the range of ages.
+    @Query(value = "SELECT * FROM user_account ua JOIN account_details ad " +
+            " ON ua.account_details_id = ad.id " +
+            " WHERE ad.age BETWEEN ?1 AND ?2", nativeQuery = true)
+    List<User> retrieveBetweenAgeRange(Integer age1, Integer age2);
+
+    // Read a user by email.
+    @Query(value = "SELECT * FROM user_account WHERE email = ?1", nativeQuery = true)
+    Optional<User> retrieveByEmail(String email);
 }
