9ae680b01f24f64c28dcf6edd8a7e6c6573cc8ef
Michael Yingling <myingling33@gmail.com>
2023-12-18 06:50:41 -0600

    Spring-15-Rest CourseController implemented.

---
+++ Spring-16-REST-Jackson/src/main/java/com/cydeo/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.cydeo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    private UserRepository userRepository;
+    private AccountRepository accountRepository;
+
+    public HomeController(UserRepository userRepository, AccountRepository accountRepository) {
+        this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> readAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/accounts")
+    public List<Account> readAllAccounts(){
+        return accountRepository.findAll();
+    }
+}
