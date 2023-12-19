fd729d411fcdc78c61172e53e53ff855e167cc17
Michael Yingling <myingling33@gmail.com>
2023-12-19 14:41:52 -0600

    Spring-16-Jackson

--- Spring-16-REST-Jackson/src/main/java/com/cydeo/entity/Account.java
+++ Spring-16-REST-Jackson/src/main/java/com/cydeo/entity/Account.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Table(name = "account_details")
-@JsonIgnoreProperties(value = "{hibernateLazyInitializer}", ignoreUnknown = true)
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer"}, ignoreUnknown = true)
 public class Account extends BaseEntity {
 
     private String name;
