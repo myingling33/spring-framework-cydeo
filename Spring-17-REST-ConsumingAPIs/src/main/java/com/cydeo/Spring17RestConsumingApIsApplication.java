b691cfb268b8a10c1688f7a369fe37c715df82b3
Michael Yingling <myingling33@gmail.com>
2023-12-21 05:42:57 -0600

    Spring-17-ConsumingAPIs

--- Spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/Spring17RestConsumingApIsApplication.java
+++ Spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/Spring17RestConsumingApIsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring17RestConsumingApIsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring17RestConsumingApIsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Spring17RestConsumingApIsApplication.class, args);
+    }
 
 }
--- Spring-17-REST-ConsumingAPIs/src/test/java/com/cydeo/Spring17RestConsumingApIsApplicationTests.java
+++ Spring-17-REST-ConsumingAPIs/src/test/java/com/cydeo/Spring17RestConsumingApIsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Spring17RestConsumingApIsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
