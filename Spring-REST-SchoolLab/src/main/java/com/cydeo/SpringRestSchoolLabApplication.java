b03e20909e92bc8a1ae0c859c2013e43aa10eddd
Michael Yingling <myingling33@gmail.com>
2024-01-06 07:37:46 -0600

    Spring REST SchoolLab added enums

---
+++ Spring-REST-SchoolLab/src/main/java/com/cydeo/SpringRestSchoolLabApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSchoolLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSchoolLabApplication.class, args);
+	}
+
+}
