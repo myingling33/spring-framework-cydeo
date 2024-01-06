fa40c1998c1196f5a701f92c1f95552353dd61fb
Michael Yingling <myingling33@gmail.com>
2024-01-06 08:17:35 -0600

    Spring REST SchoolLab added ModelMapper and AddressServiceImpl

--- Spring-REST-SchoolLab/src/main/java/com/cydeo/SpringRestSchoolLabApplication.java
+++ Spring-REST-SchoolLab/src/main/java/com/cydeo/SpringRestSchoolLabApplication.java
@@ -1,13 +1,20 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRestSchoolLabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestSchoolLabApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper mapper() {
+		return new ModelMapper();
 	}
 
 }
--- Spring-REST-SchoolLab/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
+++ Spring-REST-SchoolLab/src/main/java/com/cydeo/service/impl/AddressServiceImpl.java
@@ -1,10 +1,15 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.AddressDTO;
+import com.cydeo.entity.Address;
+import com.cydeo.repository.AddressRepository;
 import com.cydeo.service.AddressService;
+import com.cydeo.util.MapperUtil;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressServiceImpl implements AddressService {
@@ -19,21 +24,51 @@ public class AddressServiceImpl implements AddressService {
 
     @Override
     public List<AddressDTO> findAll() {
-        return null;
+
+        return addressRepository.findAll()
+                .stream()
+                .map(address -> mapperUtil.convert(address, new AddressDTO()))
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public AddressDTO findById(Long id) throws Exception {
-        return null;
+
+        Address foundAddress = addressRepository.findById(id)
+                .orElseThrow(() -> new Exception("No Address Found!"));
+        return mapperUtil.convert(foundAddress, new AddressDTO());
+
     }
 
     @Override
     public AddressDTO update(AddressDTO addressDTO) throws Exception {
-        return null;
+
+        addressRepository.findById(addressDTO.getId())
+                .orElseThrow(() -> new Exception("No Address Found!"));
+
+        Address addressToSave = mapperUtil.convert(addressDTO, new Address());
+
+        addressRepository.save(addressToSave);
+
+        return mapperUtil.convert(addressToSave, new AddressDTO());
+
     }
 
     @Override
     public AddressDTO create(AddressDTO addressDTO) throws Exception {
-        return null;
+
+        Optional<Address> foundAddress = addressRepository.findById(addressDTO.getId());
+
+        if (foundAddress.isPresent()) {
+            throw new Exception("Address Already Exists!");
+        }
+
+        Address addressToSave = mapperUtil.convert(addressDTO, new Address());
+
+        addressRepository.save(addressToSave);
+
+        return mapperUtil.convert(addressToSave, new AddressDTO());
+
     }
 }
